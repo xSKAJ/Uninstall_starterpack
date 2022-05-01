83ddbb5cc45bea3b9801135d2f25b56c3b3236d9
xSKA <technikscream@gmail.com>
2022-05-01 23:08:26 +0200

    Add new files

---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version "bodacious"
+
+games {"gta5"}
+lua54 'yes'
+server_script "@mysql-async/lib/MySQL.lua"
+server_script 'server.lua'
+shared_script '@Exile-Handler/shared/shared.lua'
---
+++ server.lua
@@ -0,0 +1,32 @@
+-- xSKA#7104
+-- xSKA#7104
+-- xSKA#7104
+-- xSKA#7104
+-- xSKA#7104
+
+RegisterCommand('starterpack', function(source, args, nazwa)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if (xPlayer.group == 'best' or xPlayer.group == 'superadmin' or xPlayer.group == 'admin' or xPlayer.group == 'moderator' or xPlayer.group == 'support' or xPlayer.group == 'trialsupport') then
+        if args[1] ~= nil then
+            if GetPlayerName(tonumber(args[1])) ~= nil then
+				xPlayer.addInventoryItem('bread', 1)
+                TriggerClientEvent('esx:showNotification', tonumber(args[1]), 'Otrzymałeś starterpack do ekwipunku!')
+                    -- Polecam zrobic sobie pod to logi
+			else
+				TriggerClientEvent('esx:showNotification', source, 'Gracz jest offline!')
+            end
+        else
+            TriggerClientEvent('esx:showNotification', source, 'Wpisz ID!')
+        end
+    else
+        TriggerClientEvent('esx:showNotification', source, 'Nie masz permisji!')
+    end
+end, false)
+
+-- xSKA#7104
+-- xSKA#7104
+-- xSKA#7104
+-- xSKA#7104
+-- xSKA#7104
+-- xSKA#7104
+-- xSKA#7104
